da0fedfbfc0e9d0dcf2e3127b109632409bbd378
Felix Kiptoo <fkiptooh@gmail.com>
2024-05-27 15:58:25 +0300

    feat:server healthcheck

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("App running");
+import Fastify from 'fastify';
+
+const server = Fastify();
+
+server.get("/healthcheck", async function(request, response){
+    return { status: "OK" }
+});
+
+(async function main() {
+    try {
+        await server.listen(3000, "0.0.0.0");
+        console.log("Server ready at http://localhost:3000");
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}());
